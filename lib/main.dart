f33506b262b30d7dd5bbd7d2dfe166c6eae0ff5c
Walid Wazini <wazini25@gmail.com>
2021-07-08 23:58:56 +0800

    2nd updates

--- lib/main.dart
+++ lib/main.dart
@@ -20,45 +20,49 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  var nameEditingController = TextEditingController();
-
+  var nameInput = TextEditingController();
+  var logoImage = 'assets/images/flutter-1.png';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(title: Text('Hello World'),backgroundColor: Colors.deepPurpleAccent,),
-      body: SingleChildScrollView(
-        child: Center(child: Column(
-          children: [
-            Text('Welcome to intro to Flutter',style: TextStyle(color: Colors.purpleAccent, fontSize: 30),),
-            SizedBox(height: 10,),
-            Image.network('https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', width: 200,),
-            Text('I hope you like it!',style: TextStyle(color: Colors.indigo, fontSize: 26)),
-            SizedBox(height: 10,),
-            Text('This is my first Flutter project'),
-            SizedBox(height: 10,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('This is first item'),
-              SizedBox(width: 10,),
-              Text('2nd Item')],),
-            SizedBox(height: 12,),
-            TextField(decoration: InputDecoration(hintText: 'Enter Your Name'), controller: nameEditingController,),
-            TextButton(onPressed: (){
-              print('Hello ${nameEditingController.text}');
-              Fluttertoast.showToast(
-                  msg: "This is ${nameEditingController.text}",
-                  toastLength: Toast.LENGTH_SHORT,
-                  gravity: ToastGravity.BOTTOM,
-                  // timeInSecForIosWeb: 1,
-                  // backgroundColor: Colors.red,
-                  // textColor: Colors.white,
-                  // fontSize: 16.0
-              );
-            }, child: Text('Press me!'))
-          ],
-        )),
-      )
+        appBar: AppBar(title: Text('Hello World'),backgroundColor: Colors.deepPurple,),
+        body: SingleChildScrollView(
+          child: Center(child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Container(child: Text('Welcome to Flutter',
+                  style: TextStyle(color: Colors.red, fontSize: 30),),
+                  decoration: BoxDecoration(border: Border.all(color: Colors.white38)),),
+                SizedBox(height: 10,),
+                Image.asset(logoImage,width: 260,),
+                Text('I hope you like it! '),
+                SizedBox(height: 10,),
+                Text('This is first Flutter App'),
+                SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('This is first item'),
+                    SizedBox(width: 20,),
+                    Text('This is second item'),
+                  ],),
+                SizedBox(height: 10,),
+                TextField(decoration: InputDecoration(hintText: 'Enter Your Name'),
+                  controller: nameInput ,
+                ),
+                TextButton(onPressed: (){
+                  // print('Hello ${nameInput.text}');
+                  Fluttertoast.showToast(
+                      msg: "This is ${nameInput.text}",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.BOTTOM,
+                  );
+                }, child: Text('Click Here!'))
+              ],
+            ),
+          )),
+        )
     );
   }
 }
